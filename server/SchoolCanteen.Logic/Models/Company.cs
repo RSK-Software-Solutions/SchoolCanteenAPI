633ad5e9916ea80a07c259b6d2e556bc638702fc
Robert Pietras <r.pietras@novatek.pl>
2023-12-01 20:46:22 +0100

    Company model has been created

---
+++ server/SchoolCanteen.Logic/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SchoolCanteen.Logic.Models;
+
+public class Company
+{
+    public Guid CompanyId { get; set; }
+    public string Name { get; set;}
+    public int Nip { get; set;}
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public List<User> Users { get; set; }
+}
