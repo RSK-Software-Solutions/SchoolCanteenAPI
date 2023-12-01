e2abbbe7aa326c3adeff07a70b6728054a9dfe14
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-12-01 20:05:41 +0100

    fix structure added gitignore

---
+++ client-side/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider
+    router={router}
+  />
+);
+
