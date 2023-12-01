424040cd769674d2982458adf98c778802ddd0f9
Robert Pietras <r.pietras@novatek.pl>
2023-12-01 20:27:18 +0100

    Models Role, User, UserDetails has been created

---
+++ server/SchoolCanteen.Logic/Models/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SchoolCanteen.Logic.Models;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public List<Role> Roles { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public UserDetails UserDetails { get; set; }
+}
---
+++ server/SchoolCanteen.Logic/Models/UserDetails.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SchoolCanteen.Logic.Models;
+
+public class UserDetails
+{
+    public int UserDetailsId { get; set; }
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string PostalCode { get; set; }
+    public string City { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
