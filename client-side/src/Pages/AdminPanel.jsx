f86c86dbd3b45a163b57a3e1641885efe700bc1a
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2023-12-14 11:59:32 +0100

    more information in adminPanel

--- client-side/src/Pages/AdminPanel.jsx
+++ client-side/src/Pages/AdminPanel.jsx
@@ -3,6 +3,22 @@ import React from 'react';
 const AdminPanel = () => {
     return (
         <div>
+            <div>
+                Zarządzanie zespołem
+                <input type="search" placeholder="Szukaj pracownika" />
+                <button>Dodaj pracownika</button>
+                <button>Usuń pracownika</button>
+                <button>Edytuj pracownika</button>
+            </div>
+
+
+            <div>Zarządzanie finansami
+
+                <p>INFORMACJE O WYDATKACH</p>
+                <p>INFORMACJE O ZAROBKACH</p>
+                <p>INFORMACJE O PRZYCHODACH</p>
+
+            </div>
         </div>
     );
 };
