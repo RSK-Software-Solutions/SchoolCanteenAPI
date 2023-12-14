ddf4a657af866af5a570f6f30ab6041270a159e6
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-12-14 10:45:12 +0100

    navbar elements added

--- client-side/src/Components/Navbar/Navbar.jsx
+++ client-side/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+    const navLinks =
+        [{
+            path: "/panel-admina", element: "Panel admina",
+        }, {
+            path: "/powiadomienia", element: "powiadomienia",
+        }, {
+            path: "/jadlospis", element: "Menu",
+        }, {
+            path: "/raporty", element: "Raporty",
+        }, {
+            path: "/ustawienia", element: "Ustawienia",
+        }];
     return (
         <div>
-            
+            {navLinks.map(el =>
+                (
+                    <Link to={el.path}>{el.element}</Link>
+                )
+            )}
         </div>
     );
 };
