3af25595e9a35a1cae1af9eddf87017e023185e6
Robert Pietras <r.pietras@novatek.pl>
2023-12-17 15:12:29 +0100

    Refactoring - User model has been changed - removed RoleId

--- server/SchoolCanteen.DATA/Models/User.cs
+++ server/SchoolCanteen.DATA/Models/User.cs
@@ -6,7 +6,6 @@ public class User
     public Guid UserId { get; set; }
     public Guid CompanyId { get; set; }
     public Guid UserDetailsId { get; set; }
-    public Guid RoleId { get; set; }
     public string Login { get; set; }
     public string Password { get; set; }
     public string? FirstName { get; set; } = string.Empty;
--- server/SchoolCanteen.Logic/DTOs/UserDTOs/SimpleUserDTO.cs
+++ server/SchoolCanteen.Logic/DTOs/UserDTOs/SimpleUserDTO.cs
@@ -3,6 +3,7 @@ namespace SchoolCanteen.Logic.DTOs.UserDTOs;
 
 public class SimpleUserDTO
 {
+    public Guid UserId { get; set; }
     public string Login { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
