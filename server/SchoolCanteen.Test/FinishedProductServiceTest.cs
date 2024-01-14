43ae98f824031537b4503f6d85281aba5e43031a
Robert Pietras <r.pietras@novatek.pl>
2024-01-14 21:30:02 +0100

    Tes project has been created, 2 test for RecipeRepository 2 test for FinishedProductService

---
+++ server/SchoolCanteen.Test/FinishedProductServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework.Internal;
+using SchoolCanteen.DATA.Models;
+using SchoolCanteen.DATA.Repositories.FinishedProductRepo;
+using SchoolCanteen.Logic.DTOs.ProductDTOs;
+using SchoolCanteen.Logic.Services.Authentication.Interfaces;
+using SchoolCanteen.Logic.Services.FinishedProductServices;
+
+namespace SchoolCanteen.Test;
+
+[TestFixture]
+public class FinishedProductServiceTest
+{
+    [Test]
+    public async Task CreateAsync_ExistingProduct_ReturnsExistingProduct()
+    {
+        // Arrange
+        var productDto = new SimpleFinishedProductDto (0, Guid.NewGuid(), "NewProduct", 1 );
+        var repositoryMock = new Mock<IFinishedProductRepository>();
+        var tokenUtilMock = new Mock<ITokenUtil>();
+        var loggerMock = new Mock<ILogger<FinishedProductService>>();
+        var mapperMock = new Mock<IMapper>();
+
+        mapperMock.Setup(mapper => mapper.Map<FinishedProduct>(It.IsAny<SimpleFinishedProductDto>()))
+            .Returns(new FinishedProduct { Name = productDto.Name });
+
+        repositoryMock.Setup(repo => repo.GetByNameAsync(productDto.Name))
+                      .ReturnsAsync(new FinishedProduct { FinishedProductId = 1, Name = productDto.Name });
+
+        var productService = new FinishedProductService(mapperMock.Object, loggerMock.Object, tokenUtilMock.Object, repositoryMock.Object);
+
+        // Act
+        var result = await productService.CreateAsync(productDto);
+
+        // Assert
+        repositoryMock.Verify(repo => repo.GetByNameAsync(productDto.Name), Times.Once);
+        repositoryMock.Verify(repo => repo.AddAsync(It.IsAny<FinishedProduct>()), Times.Never);
+        loggerMock.Verify(logger => logger.LogInformation(It.IsAny<string>()), Times.Once);
+
+        Assert.AreEqual(1, result.FinishedProductId); // Make sure the returned product is the existing one.
+    }
+    [Test]
+    public async Task CreateAsync_NonExistingProduct_CreatesNewProduct()
+    {
+        // Arrange
+        var productDto = new SimpleFinishedProductDto(0, Guid.NewGuid(), "NewProduct", 1);
+        var repositoryMock = new Mock<IFinishedProductRepository>();
+        var tokenUtilMock = new Mock<ITokenUtil>();
+        var loggerMock = new Mock<ILogger<FinishedProductService>>();
+        var mapperMock = new Mock<IMapper>();
+
+        mapperMock.Setup(mapper => mapper.Map<FinishedProduct>(It.IsAny<SimpleFinishedProductDto>()))
+          .Returns(new FinishedProduct { Name = productDto.Name }); 
+
+        repositoryMock.Setup(repo => repo.GetByNameAsync(productDto.Name))
+                      .ReturnsAsync((FinishedProduct)null);
+        
+        tokenUtilMock.Setup(t => t.GetIdentityCompany())
+             .Returns(productDto.CompanyId);
+
+        var productService = new FinishedProductService(
+            mapperMock.Object, 
+            loggerMock.Object, 
+            tokenUtilMock.Object, 
+            repositoryMock.Object);
+
+        // Act
+        var result = await productService.CreateAsync(productDto);
+
+        // Assert
+        repositoryMock.Verify(repo => repo.GetByNameAsync(productDto.Name), Times.Once);
+        repositoryMock.Verify(repo => repo.AddAsync(It.IsAny<FinishedProduct>()), Times.Once);
+        //loggerMock.Verify(logger => logger.LogInformation(It.IsAny<string>()), Times.Once);
+        //loggerMock.Verify(logger => logger.LogInformation(It.IsAny<string>(), It.IsAny<object[]>()), Times.Never);
+ 
+        Assert.AreEqual(productDto.Name, result.Name); // Make sure the returned product is the new one.
+    }
+}
---
+++ server/SchoolCanteen.Test/RecipeRepositoryTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SchoolCanteen.DATA.DatabaseConnector;
+using SchoolCanteen.DATA.Models;
+using SchoolCanteen.DATA.Repositories.RecipeRepo;
+
+namespace SchoolCanteen.Test;
+
+public class RecipeRepositoryTest
+{
+    private DatabaseApiContext dbContext;
+    private Mock<DatabaseApiContext> mockContext;
+    private Mock<ILogger<RecipeRepository>> logger;
+    private RecipeRepository recipeRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<DatabaseApiContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDb")
+            .Options;
+
+        dbContext = new DatabaseApiContext(dbContextOptions);
+        logger = new Mock<ILogger<RecipeRepository>>();
+
+        var CompanyId = Guid.NewGuid();
+        var recipes = new List<Recipe>
+        {
+            new Recipe { RecipeId = 1, Name = "Receptura 1", CompanyId = CompanyId, UnitId = 1, Quantity = 1 },
+            new Recipe { RecipeId = 2, Name = "Receptura 2", CompanyId = CompanyId, UnitId = 1, Quantity = 10 }
+        };
+
+        dbContext.Recipes.AddRange(recipes);
+        dbContext.SaveChanges();
+
+        recipeRepository = new RecipeRepository(dbContext, logger.Object);
+    }
+
+    [Test]
+    public async Task GetByIdAsync_WithExistingId_ReturnsRecipe()
+    {
+        // Act
+        var result = await recipeRepository.GetByIdAsync(2);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.AreEqual(2, result.RecipeId);
+        // Add more assertions based on your specific requirements
+    }
+}
