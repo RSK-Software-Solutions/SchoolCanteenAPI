e7f7ec3162f58d597a53b5a9e0c66d74cb8e193e
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2024-01-25 15:04:40 +0100

    feat: added CreateAsync integrationTest

--- server/SchoolCanteen.Test/UserControllerIntegrationTests.cs
+++ server/SchoolCanteen.Test/UserControllerIntegrationTests.cs
@@ -1,118 +1,147 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Configuration;
 using Moq;
-using NUnit.Framework;
-using SchoolCanteen.API;
-using SchoolCanteen.Logic.DTOs.UserDTOs;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.IdentityModel.Tokens;
 using SchoolCanteen.Logic.Services.Authentication.Interfaces;
-using SchoolCanteen.Logic.Services.User;
-using Moq;
+using SchoolCanteen.Logic.DTOs.UserDTOs;
+using SchoolCanteen.API;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+using SchoolCanteen.DATA.DatabaseConnector;
+using NUnit.Framework.Internal;
 
 namespace SchoolCanteen.Test
 {
-[TestFixture]
-public class UserControllerIntegrationTests
-{
-    private WebApplicationFactory<Startup> _factory;
+    [TestFixture]
+    public class UserControllerIntegrationTests
+    {
+        private WebApplicationFactory<Startup> _factory;
+        private JwtTokenGenerator tokenGenerator;
+        private HttpClient client;
+        private string companyId;
         private IConfiguration Configuration { get; set; }
 
-    [SetUp]
-    public void Setup()
-    {
-        _factory = new WebApplicationFactory<Startup>();
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new WebApplicationFactory<Startup>();
             var configurationBuilder = new ConfigurationBuilder()
                 .AddUserSecrets<Startup>();
 
             Configuration = configurationBuilder.Build();
-    }
+            tokenGenerator = new JwtTokenGenerator(Configuration);
+            client = SetupTestClientWithMockTokenUtil();
+            companyId = "08dc18c7-54cd-4746-8647-a6c19d7c127c";
+        }
 
-    [Test]
-    public async Task GetAllAsync_AdminRole_ReturnsOk()
-    {
-        // Arrange
-        var client = _factory.WithWebHostBuilder(builder =>
+
+        [Test]
+        public async Task GetAllAsync_AdminRole_ReturnsOk()
         {
-            builder.ConfigureTestServices(services =>
+
+            var token = tokenGenerator.GenerateJwtTokenWithAdminRole();
+
+
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+
+            var response = await client.GetAsync("/api/users");
+
+
+            response.EnsureSuccessStatusCode();
+
+            var users = await response.Content.ReadFromJsonAsync<IEnumerable<SimpleUserDTO>>();
+            Assert.NotNull(users);
+        }
+        [Test]
+        public async Task GetAllAsync_UserRole_ReturnsUnauthorize()
+        {
+
+            var token = tokenGenerator.GenerateJwtTokenWithUserRole();
+
+
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+
+            var response = await client.GetAsync("/api/users");
+
+
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+        [Test]
+        public async Task GetByNameAsync_AdminRole_ReturnsUser()
+        {
+            var token = tokenGenerator.GenerateJwtTokenWithAdminRole();
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+            var userName = "user2";
+            var response = await client.GetAsync($"/api/users?UserName={userName}");
+
+            response.EnsureSuccessStatusCode();
+
+            var users = await response.Content.ReadFromJsonAsync<IEnumerable<SimpleUserDTO>>();
+            Assert.NotNull(users);
+        }
+        [Test]
+        public async Task CreateAsync_AdminRole_CreatesUser()
+        {
+            var token = tokenGenerator.GenerateJwtTokenWithAdminRole();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+            var newUser = new CreateUserDTO
             {
-                services.AddSingleton<ITokenUtil>(provider =>
+                CompanyId = Guid.Parse(companyId),
+                UserName = "userNameTests",
+                Email = "tests@example.com",
+                Password = "password",
+                RoleName = "Admin",
+            };
+          
+            var response = await client.PostAsJsonAsync("/api/users", newUser);
+
+
+            response.EnsureSuccessStatusCode();
+
+
+            var createdUser = await response.Content.ReadFromJsonAsync<SimpleUserDTO>();
+            Assert.NotNull(createdUser);
+
+
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<UsersContext>();
+                var userInDatabase = await dbContext.Users.FirstOrDefaultAsync(u => u.Email == newUser.Email);
+                Assert.NotNull(userInDatabase);
+            }
+        }
+
+
+
+        private HttpClient SetupTestClientWithMockTokenUtil()
+        {
+            return _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
                 {
-                    services.RemoveAll<ITokenUtil>();
-                    var mock = new Mock<ITokenUtil>();
+                    services.AddSingleton<ITokenUtil>(provider =>
+                    {
+                        services.RemoveAll<ITokenUtil>();
+                        var mock = new Mock<ITokenUtil>();
 
+                        mock.Setup(util => util.GetIdentityCompany()).Returns(Guid.Parse(companyId));
 
-                    mock.Setup(util => util.GetIdentityCompany()).Returns(Guid.Parse("08dc18c7-54cd-4746-8647-a6c19d7c127c"));
+                        return mock.Object;
+                    });
 
-                    return mock.Object;
                 });
-
-            });
-        }).CreateClient();
-
-        var token = GenerateJwtTokenWithAdminRole();
-
-
-
-        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
-
-
-        var response = await client.GetAsync("/api/users");
-
-
-        response.EnsureSuccessStatusCode(); // Status Code 200-299
-
-        var users = await response.Content.ReadFromJsonAsync<IEnumerable<SimpleUserDTO>>();
-        Assert.NotNull(users);
-    }
-
-    private string GenerateJwtTokenWithAdminRole()
-    {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-            var validIssuer = config["Authentication:ValidIssuer"];
-            var validAudience = config["Authentication:ValidAudience"];
-            var claimNameSub = config["Authentication:ClaimNameSub"];
-            var issuerSigningKey = Configuration["Authentication:IssuerSigningKey"];
-
-        var claims = new List<Claim>
-        {
-            new Claim(JwtRegisteredClaimNames.Sub, claimNameSub),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ")),
-                new Claim(ClaimTypes.Role, "Admin"),
-        };
-
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Issuer = validIssuer,
-            Audience = validAudience,
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddMinutes(30), // Set expiration time
-            SigningCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
-                SecurityAlgorithms.HmacSha256
-            ),
-        };
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-
-        return tokenHandler.WriteToken(token);
-    }
+            }).CreateClient();
+        }
 
         [TearDown]
         public void TearDown()
