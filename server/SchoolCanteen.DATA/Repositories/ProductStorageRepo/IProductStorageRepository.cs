01527c96f3874dbceed841cd54b10d75852ab243
Robert Pietras <r.pietras@novatek.pl>
2024-01-28 20:37:12 +0100

    created repo delete item from finished product

---
+++ server/SchoolCanteen.DATA/Repositories/ProductStorageRepo/IProductStorageRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using SchoolCanteen.DATA.Models;
+
+namespace SchoolCanteen.DATA.Repositories.ProductStorageRepo;
+
+public interface IProductStorageRepository
+{
+    Task<bool> DeleteAsync(ProductStorage productStorage);
+}
+
