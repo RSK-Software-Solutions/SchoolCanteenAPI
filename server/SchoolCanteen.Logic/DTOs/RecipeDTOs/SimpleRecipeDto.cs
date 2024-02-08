b7d37767241ea047e7f5db20d7a2d46dca9d9ebf
Robert Pietras <r.pietras@novatek.pl>
2024-02-08 17:22:12 +0100

    fixed SimpleRecipeDto - added ValidityPeriod

--- server/SchoolCanteen.Logic/DTOs/RecipeDTOs/SimpleRecipeDto.cs
+++ server/SchoolCanteen.Logic/DTOs/RecipeDTOs/SimpleRecipeDto.cs
@@ -7,6 +7,7 @@ public class SimpleRecipeDto
     public int RecipeId { get; set; }
     public string Name { get; set; }
     public float Quantity { get; set; } = 0;
+    public int ValidityPeriod { get; set; }
     public DateTime CreatedAt { get; set; } 
     public List<SimpleRecipeDetailsDto> Details { get; set; }
 }
